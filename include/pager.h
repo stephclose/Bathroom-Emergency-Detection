002ad21cefa1062a40e0e32903d30b498908c687
stephclose <closes@purdue.edu>
2025-02-15 18:53:06 -0500

    pager

---
+++ include/pager.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define DOOR_CLOSED 0
+#define DOOR_OPEN 1
+#define MOVEMENT_DETECTED 1
+#define NO_MOVEMENT 0
+#define EMERGENCY_ALERT_TRIGGERED 1
+#define FALSE_ALARM 0
