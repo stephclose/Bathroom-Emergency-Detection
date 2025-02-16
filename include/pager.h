5a2d7699644c8b0428d3af3fa9b6c3670e8bad61
stephclose <closes@purdue.edu>
2025-02-15 19:26:02 -0500

    x

--- include/pager.h
+++ include/pager.h
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "stm32f0xx.h"
 
 #define DOOR_CLOSED 0
 #define DOOR_OPEN 1
@@ -8,3 +9,6 @@
 #define NO_MOVEMENT 0
 #define EMERGENCY_ALERT_TRIGGERED 1
 #define FALSE_ALARM 0
+
+
+void internal_clock();
