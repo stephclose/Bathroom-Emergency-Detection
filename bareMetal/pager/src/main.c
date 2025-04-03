99c722a992ab438ffb74d43e4c3358e89f86ef8a
stephclose <closes@purdue.edu>
2025-04-03 12:55:57 -0400

    test 4/3

--- bareMetal/pager/src/main.c
+++ bareMetal/pager/src/main.c
@@ -29,9 +29,31 @@ int main(void) {
     test_rfm9x_basic_communication();
     nano_wait(1000000);
     
-    fake_rx_custom_message("blahblahblbh!");
+    //fake_rx_custom_message("blahblahblbh!");
+
+    //uint8_t buffer[64];
+    //uint8_t len = rfm9x_receive_packet(buffer, sizeof(buffer));
+    //if (len > 0) {
+    //    buffer[len] = '\0';
+    //    uart_send_string("Received: ");
+    //    uart_send_string((char*)buffer);
+    //    uart_send_string("\r\n");
+    //}
+
 
     while (1) {
-        __WFI(); //sleep forever
-    }
+        uint8_t irq_flags = rfm9x_read_register(0x12); //IRQ Flags
+        if (irq_flags & 0x40) { // RX Done
+            rfm9x_write_register(0x12, 0xFF); //clear all IRQ flags
+            uint8_t buffer[64];
+            uint8_t len = rfm9x_receive_packet(buffer, sizeof(buffer));
+            buffer[len] = '\0';
+            uart_send_string("Got message: ");
+            uart_send_string((char*)buffer);
+            uart_send_string("\r\n");
+            rfm9x_set_mode(0x05);
+        }
+    __WFI();
+}
+
 }
