dd9ebec8910a124c3bc1b30810efb30cf218a99f
stephclose <closes@purdue.edu>
2025-04-04 12:43:35 -0400

    tx/rx

--- bareMetal/pager/src/main.c
+++ bareMetal/pager/src/main.c
@@ -22,12 +22,59 @@ int main(void) {
     uart_init();
     pc7_led_init();
 
+    //TESTING TRANSMITTING CODE
     rfm9x_reset();
     init_spi1_lora();
-    test_rfm9x_registers();
-    nano_wait(1000000);
-    test_rfm9x_basic_communication();
-    nano_wait(1000000);
+    rfm9x_set_frequency(915);
+    rfm9x_set_tx_power(14);
+    rfm9x_set_mode(0x01); 
+    
+    uart_send_string("Transmitter ready!\r\n");
+    const char *msg = "Hello from STM32 TX!";
+    uint8_t buffer[64];
+    strcpy((char*)buffer, msg);
+
+    while (1) {
+        uart_send_string("Sending packet...\r\n");
+        rfm9x_send_packet(buffer, strlen((char*)buffer));
+        uart_send_string("Sent!\r\n");
+        nano_wait(1000000000);
+    }
+
+    //RECIEVING CODE
+    rfm9x_reset();
+    init_spi1_lora();
+    rfm9x_set_frequency(915);
+    rfm9x_set_mode(0x05);
+
+    uart_send_string("Receiver listening...\r\n");
+
+    while (1) {
+        uint8_t irq_flags = rfm9x_read_register(0x12);
+        if (irq_flags & 0x40) {
+            rfm9x_write_register(0x12, 0xFF);
+
+            uint8_t buffer[64];
+            uint8_t len = rfm9x_receive_packet(buffer, sizeof(buffer));
+            buffer[len] = '\0';
+
+            uart_send_string("Got message: ");
+            uart_send_string((char*)buffer);
+            uart_send_string("\r\n");
+
+            rfm9x_set_mode(0x05);
+        }
+
+        __WFI();
+    }
+}
+
+    //rfm9x_reset();
+    //init_spi1_lora();
+    //test_rfm9x_registers();
+    //nano_wait(1000000);
+    //test_rfm9x_basic_communication();
+    //nano_wait(1000000);
     
     //fake_rx_custom_message("blahblahblbh!");
 
@@ -41,7 +88,7 @@ int main(void) {
     //}
 
 
-    while (1) {
+    /*while (1) {
         uint8_t irq_flags = rfm9x_read_register(0x12); //IRQ Flags
         if (irq_flags & 0x40) { // RX Done
             rfm9x_write_register(0x12, 0xFF); //clear all IRQ flags
@@ -54,6 +101,7 @@ int main(void) {
             rfm9x_set_mode(0x05);
         }
     __WFI();
-}
+    }*
+
 
 }
